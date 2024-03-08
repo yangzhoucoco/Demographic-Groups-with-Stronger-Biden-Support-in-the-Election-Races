e08ef4d6a54c7872ae76f2af7fe38e3184648c04
yangzhoucoco <yangzhou@YangdeMacBook-Air.local>
2024-03-08 00:39:56 -0500

    update simulated_data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,10 +10,29 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(853)
 
+num_obs <- 1000
 
+us_political_preferences <- tibble(
+  education = sample(0:4, size = num_obs, replace = TRUE),
+  gender = sample(0:1, size = num_obs, replace = TRUE),
+  support_prob = ((education + gender) / 5),
+) |>
+  mutate(
+    supports_biden = if_else(runif(n = num_obs) < support_prob, "yes", "no"),
+    education = case_when(
+      education == 0 ~ "< High school",
+      education == 1 ~ "High school",
+      education == 2 ~ "Some college",
+      education == 3 ~ "College",
+      education == 4 ~ "Post-grad"
+    ),
+    gender = if_else(gender == 0, "Male", "Female")
+  ) |>
+  select(-support_prob, supports_biden, gender, education)
 
+write.csv(us_political_preferences, "data/simulated_data/simulated_data.csv")
