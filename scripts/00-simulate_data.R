8374e371c55f10a78d48ae03a32bc0b3222292bb
yangzhoucoco <cocoyang.zhou@mail.utoronto.ca>
2024-03-09 10:33:11 -0500

    update simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,6 +1,6 @@
 #### Preamble ####
 # Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
+# Author: Yang Zhou & Yuean Wang [...UPDATE THIS...]
 # Date: 11 February 2023 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
 # License: MIT
@@ -15,27 +15,27 @@ library(dplyr)
 #### Simulate data ####
 set.seed(853)
 
-num_obs <- 1000
+num_obs <- 800
 
 us_political_preferences <- tibble(
-  education = sample(0:4, size = num_obs, replace = TRUE),
-  gender = sample(0:1, size = num_obs, replace = TRUE),
-  support_prob = ((education + gender) / 5),
+  race = sample(0:5, size = num_obs, replace = TRUE),
+  lgbtq = sample(0:1, size = num_obs, replace = TRUE),
+  support_prob = ((race + lgbtq) / 5),
 ) |>
   mutate(
     supports_biden = if_else(runif(n = num_obs) < support_prob, "yes", "no"),
-    education = case_when(
-      education == 0 ~ "< High school",
-      education == 1 ~ "High school",
-      education == 2 ~ "Some college",
-      education == 3 ~ "College",
-      education == 4 ~ "Post-grad"
+    race = case_when(
+      race == 0 ~ "White",
+      race == 1 ~ "Black",
+      race == 2 ~ "Hispanic",
+      race == 3 ~ "Asian",
+      race == 4 ~ "Other"
     ),
-    gender = if_else(gender == 0, "Male", "Female")
+    lgbtq = if_else(lgbtq == 0, "LGBTQ", "Non_LGBTQ")
   ) |>
-  select(-support_prob, supports_biden, gender, education)
+  select(-support_prob, supports_biden, race, lgbtq)
 
 
 
 
-write.csv(us_political_preferences, "data/simulated_data/simulated_data.csv")
+write.csv(us_political_preferences, "data/simulate_data/simulate_data.csv")
