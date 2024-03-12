9ba4456d11d81241327230f8d531fba18703c043
DJY1231 <156145192+DJY1231@users.noreply.github.com>
2024-03-11 22:08:24 -0400

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
 # Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Date: 12 March 2024 
+# Contact: cocoyang.zhou@utoronto.ca & dongjun.yoon@mail.utoronto.ca & yuean.wang@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: No
 # Any other information needed? No
